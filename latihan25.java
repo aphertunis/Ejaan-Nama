99da7c04861cb6510a4d82c74c5c18a1056f9e59
Nur Riskon Abdan Syakuro <23176031@mhs.stmik-tegal.ac.id>
2024-11-04 19:00:44 +0700

    Add files via upload

---
+++ latihan25.java
@@ -0,0 +1,21 @@
+//Nama      : Nur Riskon Abdan Syakuro
+//  NIM       : 23176031
+//  Prodi     : Sistem Informasi
+//  Deskripsi :   Program ini untuk mengeja nama yang diinputkan user
+
+
+import java.util.Scanner;
+
+public class latihan25 {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Masukkan nama depan anda untuk di eja: ");
+            String nama = scanner.nextLine();
+            
+            System.out.println("Ejaan untuk \"" + nama + "\" adalah:");
+            for (int i = 0; i < nama.length(); i++) {
+                System.out.println("Huruf ke-" + (i + 1) + " : " + nama.charAt(i));
+            }
+        }
+    }
+}
